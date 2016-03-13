c88377e4b05889466a969e975dcfe5de117f3d39
MichelKansou <michel.kansou@outlook.fr>
2016-03-13 20:55:50 +0100

    2.0 Version Launch
    My Resume with React & Semantic UI

---
+++ resources/js/Views/Footer.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var T = require('i18n-react');
+
+var Footer = React.createClass({
+    render: function() {
+      var trans = this.props.trans;
+      return (
+          <div className="ui inverted vertical footer segment custom-footer">
+              <div className="ui text container footer-container">
+                  <div className="segment-title">
+                      <i className="fa fa-paper-plane"> - <T text={trans + 'Footer.contact.title'}/> </i>
+                  </div>
+                  <T text={trans + 'Footer.contact.message'}/>
+                  <div className="social-container">
+                      <a href="mailto:michel.kansou@viacesi.fr"><i className="circular inverted yellow big mail outline icon"></i></a>
+                      <a href="https://www.facebook.com/michel.kansou"><i className="circular inverted blue big facebook icon"></i></a>
+                      <a href="https://twitter.com/MichelKansou"><i className="circular inverted blue big twitter icon"></i></a>
+                      <a href="https://fr.linkedin.com/in/michel-kansou-4384a38a"><i className="circular inverted blue big linkedin icon"></i></a>
+                      <a href="http://stackoverflow.com/users/4724949/michel"><i className="circular inverted grey big stack overflow icon"></i></a>
+                      <a href="https://github.com/MichelKansou"><i className="circular inverted red big github icon"></i></a>
+                  </div>
+                  <a className="ui orange button resume-download" href={"resources/CV/Michel Kansou CV-"+trans+"pdf"} >
+                      <i className="fa fa-download"></i> <T text={trans + 'Profile.downloadCV'}/>
+                        </a>
+                <p id="copyright"><i className="fa fa-code"></i> with <i className="fa fa-heart"></i> <T text={trans + 'Footer.copyright'}/></p>
+              </div>
+        </div>
+      );
+    }
+});
+
+export default Footer;
---
+++ resources/js/Views/NavigationMenu.jsx
@@ -0,0 +1,79 @@
+var React = require('react');
+var T = require('i18n-react');
+var $ = require('jquery');
+var scrollMonitor = require("scrollMonitor");
+$.fn.dropdown = require('semantic-ui-dropdown');
+$.fn.transition = require('semantic-ui-transition');
+
+var NavigationMenu = React.createClass({
+    getInitialState: function() {
+        return {
+            NavTitle: 'My Resume'
+        };
+    },
+    componentDidMount() {
+        var profileWatcher = scrollMonitor.create( document.getElementById("profile") );
+        var skillsWatcher = scrollMonitor.create( document.getElementById("skills") );
+        var experienceWatcher = scrollMonitor.create( document.getElementById("experience") );
+        var educationWatcher = scrollMonitor.create( document.getElementById("education") );
+        var portfolioWatcher = scrollMonitor.create( document.getElementById("portfolio") );
+        var contactWatcher = scrollMonitor.create( document.getElementById("contact") );
+        $('.ui.dropdown').dropdown();
+        profileWatcher.fullyEnterViewport(function() {
+            this.setState({ NavTitle: 'Profile' })
+        }.bind(this));
+        skillsWatcher.fullyEnterViewport(function() {
+            this.setState({ NavTitle: 'Skills' })
+        }.bind(this));
+        experienceWatcher.fullyEnterViewport(function() {
+            this.setState({ NavTitle: 'Experience' })
+        }.bind(this));
+        educationWatcher.fullyEnterViewport(function() {
+            this.setState({ NavTitle: 'Education' })
+        }.bind(this));
+        portfolioWatcher.fullyEnterViewport(function() {
+            this.setState({ NavTitle: 'Portfolio' })
+        }.bind(this));
+        contactWatcher.fullyEnterViewport(function() {
+            this.setState({ NavTitle: 'Contact' })
+        }.bind(this));
+    },
+    render: function() {
+      var trans = this.props.trans;
+      return (
+          <header className="headerNav">
+              <div className="ui floating dropdown labeled icon button language-button">
+                  <i className="world icon"></i>
+                  <span className="text">English</span>
+                  <div className="menu">
+                      <div className="item" onClick={this.props.handleClick.bind(null, 'EN.')}>English</div>
+                      <div className="item" onClick={this.props.handleClick.bind(null, 'FR.')} >Français</div>
+                  </div>
+              </div>
+          <div className="wrap" aria-expanded="true">
+              <h2 className="logo"><a>{this.state.NavTitle}</a></h2>
+            <a id="menu-icon">&#9776; Menu</a>
+            <nav className="navbar">
+              <ul className="menu">
+                  <li><a href="#profile"> <T text={trans + 'Navigation.profile'}/> </a></li>
+                  <li><a href="#skills"> <T text={trans + 'Navigation.skills'}/> </a></li>
+                  <li><a href="#experience"> <T text={trans + 'Navigation.workExperience'}/> </a></li>
+                  <li><a href="#education"> <T text={trans + 'Navigation.education'}/> </a></li>
+                  <li><a href="#portfolio"> <T text={trans + 'Navigation.portfolio'}/> </a></li>
+                  <li><a href="#contact"> <T text={trans + 'Navigation.contact'}/> </a></li>
+                  <li className="language-responsive-button"><a onClick={this.props.handleClick.bind(null, 'EN.')}>
+                      <T text={trans + 'Navigation.english'}/>
+                  </a></li>
+                  <li className="language-responsive-button"><a onClick={this.props.handleClick.bind(null, 'FR.')}>
+                      <T text={trans + 'Navigation.french'}/>
+                  </a></li>
+              </ul>
+            </nav>
+
+          </div>
+        </header>
+      );
+    }
+});
+
+export default NavigationMenu;
