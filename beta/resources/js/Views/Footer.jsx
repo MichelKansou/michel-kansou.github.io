89012c87de0d29cc71c816c7ff3d83fa4f33e8fe
MichelKansou <michel.kansou@outlook.fr>
2016-03-13 20:51:10 +0100

    2.0 Version
    Beta Test

--- beta/resources/js/Views/Footer.jsx
+++ beta/resources/js/Views/Footer.jsx
@@ -19,7 +19,7 @@ var Footer = React.createClass({
                       <a href="http://stackoverflow.com/users/4724949/michel"><i className="circular inverted grey big stack overflow icon"></i></a>
                       <a href="https://github.com/MichelKansou"><i className="circular inverted red big github icon"></i></a>
                   </div>
-                  <br/><a className="ui orange button resume-download" href={"resources/CV/Michel Kansou CV-"+trans+"pdf"} >
+                  <a className="ui orange button resume-download" href={"resources/CV/Michel Kansou CV-"+trans+"pdf"} >
                       <i className="fa fa-download"></i> <T text={trans + 'Profile.downloadCV'}/>
                         </a>
                 <p id="copyright"><i className="fa fa-code"></i> with <i className="fa fa-heart"></i> <T text={trans + 'Footer.copyright'}/></p>
